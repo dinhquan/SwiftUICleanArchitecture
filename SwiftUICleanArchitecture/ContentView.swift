bda29ad2a613e6897e30a0280b661d5b4d581b0b
Quan Nguyen <dinhquan191@gmail.com>
2021-02-23 15:15:06 +0700

    Add files

---
+++ SwiftUICleanArchitecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Quan on 2/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICleanArchitecture/SwiftUICleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICleanArchitectureApp.swift
+//  SwiftUICleanArchitecture
+//
+//  Created by Quan on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
