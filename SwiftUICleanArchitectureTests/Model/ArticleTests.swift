3bae7d6bab2f2e6481a323bb94862d2ff3d405cc
Quan Nguyen <dinhquan191@gmail.com>
2021-10-06 17:45:30 +0700

    Enable code coverage

--- SwiftUICleanArchitectureTests/Model/ArticleTests.swift
+++ SwiftUICleanArchitectureTests/Model/ArticleTests.swift
@@ -16,5 +16,19 @@ class ArticleTest: XCTestCase {
         
         XCTAssertEqual(article.formattedPublishedAt, "Updated: Jan 04, 09:37")
     }
+
+    func testformattedPublishedAt_invalidDate() async {
+        var article = Article()
+        article.publishedAt = "2021-02-04"
+
+        XCTAssertEqual(article.formattedPublishedAt, "")
+    }
+
+    func testId_validId() async {
+        var article = Article()
+        article.title = "test id"
+
+        XCTAssertEqual(article.id, "test id")
+    }
 }
 
